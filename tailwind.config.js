2addfb04d8affaa7e27a7ec8152552e55ab9dcd8
Sahil soni <sonisahil1018@gmail.com>
2025-02-13 15:01:45 +0530

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html"], // Ensure Tailwind scans your HTML files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
